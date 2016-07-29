a8360864a8b5d6e268261411452001e4c302df5a
FabianGosebrink <1oSzfcWKTk9raYjytkAT>
2016-07-29 18:22:34 +0200

    removing unused sampledata

--- src/SampleWebApiAspNetCore/Startup.cs
+++ src/SampleWebApiAspNetCore/Startup.cs
@@ -30,14 +30,6 @@ namespace SampleWebApiAspNetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            List<HouseEntity> houses = new List<HouseEntity>()
-            {
-                new HouseEntity() {City = "Town1", Id = 1, Street = "Street1", ZipCode = 1234},
-                new HouseEntity() {City = "Town2", Id = 2, Street = "Street2", ZipCode = 5678},
-                new HouseEntity() {City = "Town3", Id = 3, Street = "Street3", ZipCode = 9012},
-                new HouseEntity() {City = "Town4", Id = 4, Street = "Street4", ZipCode = 3456}
-            };
-
             services.AddSingleton<IHouseRepository, HouseRepository>();
 
             services.AddTransient<IHouseMapper, HouseMapper>();
