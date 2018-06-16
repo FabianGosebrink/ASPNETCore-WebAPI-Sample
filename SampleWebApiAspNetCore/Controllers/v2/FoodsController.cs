e29353e0e0ca95f0914ab8b5a9cc9344d0a498ba
FabianGosebrink <fabian.gosebrink@outlook.com>
2018-06-16 16:00:36 +0200

    small changes

--- SampleWebApiAspNetCore/Controllers/v2/FoodsController.cs
+++ SampleWebApiAspNetCore/Controllers/v2/FoodsController.cs
@@ -3,7 +3,7 @@
 namespace SampleWebApiAspNetCore.v2.Controllers
 {
     [ApiVersion("2.0")]
-    [Route("api/v{version:apiVersion}/foods")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     public class FoodsController : ControllerBase
     {
         [HttpGet]
